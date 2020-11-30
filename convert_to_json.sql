806bb34b066e1e28cb07a93431c72c59e32951bd
Dominik <dominikvasko96@gmail.com>
2020-11-30 21:43:00 +0100

    Update scripts

--- convert_to_json.sql
+++ convert_to_json.sql
@@ -1,8 +1,39 @@
--- (aid, accounts json)
-DROP TABLE IF EXISTS accounts_json;
+-- Utils
+DROP PROCEDURE IF EXISTS drop_tmp_json_tables();
+CREATE PROCEDURE drop_tmp_json_tables()
+LANGUAGE SQL
+AS $$
+   DROP TABLE IF EXISTS
+   	parentless_tweets_json,
+	accounts_by_mention_json,
+	hashtags_by_mention_json,
+	accounts_json;
+$$;
 
-CREATE TABLE accounts_json(id  bigint primary key, jsn JSON);
+DROP PROCEDURE IF EXISTS drop_all_json_tables();
+CREATE PROCEDURE drop_all_json_tables()
+LANGUAGE SQL
+AS $$
+   CALL drop_tmp_json_tables();
+   DROP TABLE IF EXISTS tweets_json;
+$$;
 
+DROP PROCEDURE IF EXISTS create_all_json_tables();
+CREATE PROCEDURE create_all_json_tables()
+LANGUAGE SQL
+AS $$
+   CREATE TABLE accounts_json(id  bigint primary key, jsn JSON);
+   CREATE TABLE hashtags_by_mention_json(tweet_id varchar, jsn json);
+   CREATE TABLE accounts_by_mention_json(tweet_id varchar, jsn json);
+   CREATE TABLE parentless_tweets_json(tweet_id varchar, jsn JSON);
+   CREATE TABLE tweets_json(id SERIAL PRIMARY KEY,	jsn JSON);
+$$;
+
+-- MAIN
+CALL drop_all_json_tables();
+CALL create_all_json_tables();
+
+-- Convert accounts to json 
 INSERT INTO accounts_json (id, jsn)
     SELECT
         id,
@@ -16,48 +47,32 @@ INSERT INTO accounts_json (id, jsn)
                	'statuses_count', statuses_count))
     FROM accounts;
 
--- (twid, uniq json_hashtags array)
-DROP TABLE IF EXISTS hashtags_by_mention_json;
+-- Aggregate accounts by mentions
+INSERT INTO accounts_by_mention_json
+        SELECT tm.tweet_id,
+       	    json_agg(DISTINCT a.jsn::jsonb)
+        FROM tweet_mentions tm
+           JOIN accounts_json a on tm.account_id = a.id
+        GROUP BY tm.tweet_id;
 
-CREATE TABLE hashtags_by_mention_json(tweet_id varchar, jsn json);
-
+-- Aggregate hashtags by mentions
 INSERT INTO hashtags_by_mention_json
        SELECT th.tweet_id,
        	      json_agg(DISTINCT h.value)
        FROM tweet_hashtags th
-       JOIN hashtags h on th.hashtag_id = h.id
+       	    JOIN hashtags h on th.hashtag_id = h.id
        GROUP BY th.tweet_id;
 
--- (twid, accounts which mention the tweet)
-DROP TABLE accounts_by_mention_json;
-
-CREATE TABLE accounts_by_mention_json(tweet_id varchar, jsn json);
-
-INSERT INTO accounts_by_mention_json
-SELECT tm.tweet_id,
-       json_agg(a.jsn)
-  FROM tweet_mentions tm
-       JOIN accounts_json a on tm.account_id = a.id
- GROUP BY tm.tweet_id;
-
--- Parentless Tweets
-DROP TABLE parentless_tweets_json;
-
-CREATE TABLE parentless_tweets_json
-(
-    tweet_id varchar,
-    jsn      json
-);
-
+-- Parentless(Non-retweet) Tweets
 INSERT INTO parentless_tweets_json
 SELECT t.id,
 	json_strip_nulls(
 		json_build_object(
-		        'content', replace(t.content, '\n', ' '),
+			'content', replace(trim(both '"' from t.content), '\n', ' '),
 		        'author', aj.jsn,
 		        'hashtags', hbmj.jsn,
 		        'mentions', abmj.jsn,
-			'location', ST_AsGeoJSON(ST_AsText(t.location)),
+			'location', ST_AsGeoJSON(ST_AsText(t.location))::json,
 			'retweet_count', t.retweet_count,
 			'favorite_count', t.favorite_count,
 			'happended_at', t.happended_at,
@@ -70,15 +85,7 @@ FROM tweets t
          LEFT JOIN accounts_json aj on t.author_id = aj.id
 WHERE t.parent_id IS NULL;
 
--- Tweets with parents
-DROP TABLE IF EXISTS tweets_json;
-
-CREATE TABLE tweets_json
-(
-    id SERIAL PRIMARY KEY,	-- For effective pagination
-    jsn json
-);
-
+-- All Tweets
 INSERT INTO tweets_json (jsn)
 SELECT
 	json_strip_nulls(
@@ -100,3 +107,5 @@ FROM tweets t
          LEFT JOIN accounts_json aj on t.author_id = aj.id
 	 LEFT JOIN countries c on t.country_id = c.id
          LEFT JOIN parentless_tweets_json ptj on t.parent_id = ptj.tweet_id;
+
+CALL drop_tmp_json_tables();
--- insert.sql
+++ insert.sql
@@ -2,69 +2,150 @@
 \i elastic_api.sql
 \i convert_to_json.sql
 
--- DROP unneeded temporary tables
-DROP TABLE IF EXISTS parentless_tweets_json;
-DROP TABLE IF EXISTS accounts_by_mention_json;
-DROP TABLE IF EXISTS hashtags_by_mention_json;
-DROP TABLE IF EXISTS accounts_json;
-
 -- BULK INSERT FROM TWEETS
 DROP TABLE IF EXISTS tmp;
-CREATE UNLOGGED TABLE tmp
-(
-    request TEXT
-);
+CREATE TABLE tmp(request TEXT);
 
 DO
 $$
     DECLARE
-        tweet_count	BIGINT = (SELECT max(id) FROM tweets_json);
-        batch_size 	INT    = 100;
+	min_id		BIGINT = (SELECT min(id) FROM tweets_json);
+        max_id		BIGINT = (SELECT max(id) FROM tweets_json);
+        batch_size 	INT    = 1000;
     BEGIN
-        FOR iter IN 0..tweet_count BY batch_size
-            LOOP
+        FOR iter IN min_id..max_id BY batch_size
+	    LOOP
 		INSERT INTO tmp (request)
 		       SELECT
+				E'{ "create": { "_index": "tmp" } }\n' ||
 				string_agg(
-				  jsn::TEXT,
-                                  E'\n' || '{ "index": { "_index": "tweets" } }' || E'\n')
+				  jsn::TEXT, E'\n{ "create": { "_index": "tmp" } }\n') ||
+				E'\n'
                        FROM (
 		       	    SELECT jsn
                             FROM tweets_json
 			    WHERE id >= iter
-			    LIMIT batch_size) AS j;
-		RAISE NOTICE 'Requests created % of % items.', iter + batch_size, tweet_count;
+			    ORDER BY id ASC
+			    LIMIT batch_size-1) AS j;
+
+		IF iter + batch_size - 1 > max_id THEN
+		   RAISE NOTICE 'Requests created % of % items.', max_id, max_id;
+		ELSE
+		   RAISE NOTICE 'Requests created % of % items.', iter + batch_size - 1, max_id;
+		END IF;
+		
             END LOOP;
-	    UPDATE tmp SET request = '{ "index": { "_index": "tweets" } }' || E'\n' || request || E'\n';
     END;
+$$;
+
+CREATE FUNCTION es_bad_response(response HTTP_RESPONSE)
+RETURN BOOLEAN
+LANGUAGE PLPGSQL
+AS $$
+   IF (response.status)::TEXT LIKE '200' THEN
+      RETURN FALSE;
+   END IF;
+
+   RETURN TRUE;
+$$;
+
+CREATE PROCEDURE es_show_res_error(response HTTP_RESPONSE)
+LANGUAGE PLPGSQL
+AS $$
+   RAISE EXCEPTION 'Bad elastic response: %, msg: %', response.status, response.content
+   	 USING HINT = 'Fix the error then rerun the insert script';
+$$;
+
+CREATE FUNCTION es_bulk_has_errors(response HTTP_RESPONSE)
+RETURN BOOLEAN
+LANGUAGE PLPGSQL
+AS $$
+   IF ((response.content::json)->'errors')::TEXT LIKE 'true' THEN
+      RETURN TRUE;
+   END IF;
+
+   RETURN FALSE;
+$$;
+
+CREATE FUNCTION es_bulk_get_error_items(response HTTP_RESPONSE)
+RETURN TABLE (json JSON)
+LANGUAGE PLPGSQL
+AS $$
+   RETURN
+	SELECT json
+	FROM json_array_elements((response.content::json)->'items') as json
+	WHERE (json->'index'->'status')::TEXT NOT LIKE '201';
+$$;
+
+CREATE PROCEDURE es_bulkitem_get_error(item RECORD)
+LANGUAGE PLPGSQL
+AS $$
+   RAISE NOTICE 'Error creating item: %', item.json->'index'->'error';
+$$;
+
+CREATE FUNCTION es_send_bulk(request TEXT)
+RETURN HTTP_RESPONSE
+LANGUAGE PLPGSQL
+AS $$
+   RETURN http_post('172.17.0.1:9200/_bulk', request, 'application/x-ndjson');
 $$;
 
 DO
 $$
     DECLARE
         req TEXT;
-	max INT = (select count(*) from tmp);
-	cur INT = 0;
-	sts TEXT;
+	item RECORD;
+	res HTTP_RESPONSE;
     BEGIN
-        FOR req IN SELECT request FROM tmp
+        FOR req IN SELECT request FROM tmp ORDER BY LENGTH(request) DESC -- Big batches are more likely to fail
             LOOP
-		cur := cur + 1;
+		res := es_send_bulk(req);
 
-		sts := (SELECT status
-                     FROM http_post(
-                             '172.17.0.1:9200/_bulk',
-                             req,
-                             'application/x-ndjson'));
+		IF es_bad_res(res) THEN
+		   es_show_res_error(res);
+		END IF;
 
-		if sts != '200' then
-		   RAISE NOTICE '%/%', cur, max;
-		   RAISE NOTICE '%', sts;
-		end if;
+		IF es_bulk_has_errors(res) THEN
+		   FOR item IN es_bulk_get_error_items(res) LOOP
+		       es_bulkitem_get_error(item);
+		   END LOOP;
+		END IF;
 
+            END LOOP;
+    END;
+$$;
+
+
+DO
+$$
+    DECLARE
+        req TEXT;
+	element RECORD;
+	response HTTP_RESPONSE;
+    BEGIN
+        FOR req IN SELECT request FROM tmp ORDER BY LENGTH(request) DESC
+            LOOP
+		-- PERFORM pg_sleep(.01);
+
+		response := http_post('172.17.0.1:9200/_bulk', req, 'application/x-ndjson');
+
+		IF (response.status)::TEXT NOT LIKE '200' THEN
+		   RAISE NOTICE '%', response.content;
+		END IF;
+
+		IF ((response.content::json)->'errors')::TEXT LIKE 'true' THEN
+		   FOR element IN SELECT json FROM json_array_elements((response.content::json)->'items') as json
+		       LOOP
+			IF (element.json->'index'->'status')::TEXT NOT LIKE '201' THEN
+			   RAISE NOTICE '%', element.json->'index'->'error';
+			END IF;
+		       END LOOP;
+		END IF;
             END LOOP;
     END;
 $$;
 
 DROP TABLE IF EXISTS tweets_json;
 DROP TABLE IF EXISTS tmp;
+
+-- Geolocation debug
